273e74233510e558dc90623b3b5a170382229f16
tiniscule <114822071+tiniscule@users.noreply.github.com>
2025-08-14 13:51:12 -0500

    working on releases and first release

--- main.ts
+++ main.ts
@@ -1,85 +1,86 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
+import { StateField, RangeSetBuilder, EditorSelection } from '@codemirror/state';
+import { Decoration, DecorationSet, EditorView } from '@codemirror/view';
 
-// Remember to rename these classes and interfaces!
+const revealDeco = Decoration.mark({ class: 'privacy-mode-reveal' });
 
-interface MyPluginSettings {
-	mySetting: string;
+const revealSelectionFontField: StateField<DecorationSet> = StateField.define<DecorationSet>({
+    create() {
+        return Decoration.none;
+    },
+    update(decorations, transaction) {
+        if (!transaction.selection || !transaction.selection.eq(transaction.startState.selection) || transaction.docChanged) {
+            const builder = new RangeSetBuilder<Decoration>();
+            const selection: EditorSelection = transaction.state.selection;
+
+            for (const range of selection.ranges) {
+                if (!range.empty) builder.add(range.from, range.to, revealDeco);
+            }
+            return builder.finish();
+        }
+        return decorations.map(transaction.changes);
+    },
+    provide: field => EditorView.decorations.from(field),
+});
+
+interface PrivacyModeSettings {
+	enabled: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PrivacyModeSettings = {
+	enabled: false
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PrivacyModePlugin extends Plugin {
+	settings: PrivacyModeSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		// Apply initial state
+		this.applyPrivacyModeClass(this.settings.enabled);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// Register CodeMirror extension to reveal real font for selected ranges
+		this.registerEditorExtension(revealSelectionFontField);
 
-		// This adds a simple command that can be triggered anywhere
+		// Command: Enable Privacy Mode
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+			id: 'enable-privacy-mode',
+			name: 'Enable',
+			checkCallback: (checking) => {
+				if (this.settings.enabled) return false;
+				if (!checking) {
+					this.settings.enabled = true;
+					this.applyPrivacyModeClass(true);
+					this.saveSettings();
 				}
+				return true;
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		// Command: Disable Privacy Mode
+		this.addCommand({
+			id: 'disable-privacy-mode',
+			name: 'Disable',
+			checkCallback: (checking) => {
+				if (!this.settings.enabled) return false;
+				if (!checking) {
+					this.settings.enabled = false;
+					this.applyPrivacyModeClass(false);
+					this.saveSettings();
+				}
+				return true;
+			}
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
+		this.applyPrivacyModeClass(false);
+	}
 
+	private applyPrivacyModeClass(enabled: boolean) {
+		const root = this.app.workspace.containerEl;
+		root.toggleClass('privacy-mode-enabled', enabled);
 	}
 
 	async loadSettings() {
@@ -88,47 +89,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
